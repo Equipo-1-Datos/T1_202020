922ce313ab0907e5d645f1b0f0e173f4a991eb3d
Your Name <c.jimenezv@uniandes.edu.co>
2020-08-20 16:04:54 -0500

    Primer commit

--- src/model/logic/Peliculas.java
+++ src/model/logic/Peliculas.java
@@ -1,5 +1,36 @@
 package model.logic;
 
+import java.io.FileReader;
+import java.util.Date;
+import java.util.Map;
+
+import com.opencsv.CSVReaderHeaderAware;
+
 public class Peliculas {
+	
+	private int id;
+	private int budget;
+	private String genero;
+	private String imdb_id;
+	private String lengua;
+	private String titulo;
+	private String overview;
+	private double popularidad;
+	private String companiaProd;
+	private String paisProd;
+	private Date fechaSalida;
+	private int revenue;
+	private int runtime;
+	private String lenguasHabladas;
+	private String status;
+	
+	/// no se sabe 
+	private String tagline;
+	
+	
+	
+	
+	
+	Map<String, String> values = new CSVReaderHeaderAware(new FileReader("yourfile.csv")).readMap();
 
 }
