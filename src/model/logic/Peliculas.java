df7a9707a1ea75fcd4c1e95da188f4f29f77fea5
Jhorman <jo.caballero@uniandes.edu.co>
2020-08-20 17:49:24 -0500

    Todos los atributos de Pelicula

--- src/model/logic/Peliculas.java
+++ src/model/logic/Peliculas.java
@@ -8,24 +8,115 @@ import com.opencsv.CSVReaderHeaderAware;
 
 public class Peliculas {
 	
+	/*
+	 * Idetificador de la pelicula. 
+	 */
 	private int id;
+	
+	/*
+	 * Presupuesto de la pelicula. 
+	 */
 	private int budget;
+	
+	/*
+	 * Genero de la pelicula. 
+	 */
 	private String genero;
+	
+	/*
+	 * imdb id 
+	 */
 	private String imdb_id;
-	private String lengua;
-	private String titulo;
-	private String overview;
+	
+	/*
+	 * Lenguaje original de la pelicula
+	 */
+	private String lenguajeOriginal;
+	
+	/*
+	 * Titulo original de la pelicula.
+	 */
+	private String tituloOriginal;
+	
+	/*
+	 * Sinopsis de la pelicula.
+	 */
+	private String sinopsis;
+	
+	/*
+	 * Popularidad de la pelicula. 
+	 */
 	private double popularidad;
-	private String companiaProd;
-	private String paisProd;
+	
+	/*
+	 * Companias de produccion de la pelicula. 
+	 */
+	private String companiasProd;
+	
+	/*
+	 * Paises de produccion de la pelicula
+	 */
+	private String paisesProd;
+	
+	/*
+	 * Fecha de salida de la pelicula. 
+	 */
 	private Date fechaSalida;
+	
+	/*
+	 * Ganancias por la pelicula. 
+	 */
 	private int revenue;
+	
+	/*
+	 * Duracion de la pelicula.
+	 */
 	private int runtime;
+	
+	/*
+	 * Lenguajes hablados en la pelicula.
+	 */
 	private String lenguasHabladas;
+	
+	/*
+	 * Estado de la pelicula.
+	 */
 	private String status;
 	
-	/// no se sabe 
+	/*
+	 * Slogan de la pelicula. 
+	 */
 	private String tagline;
+	
+	/*
+	 * Titulo de la pelicula. 
+	 */
+	private String titulo;
+	
+	/*
+	 * Promedio de valoracion.
+	 */
+	private double voteAverage;
+	
+	/*
+	 * Numero de votos. 
+	 */
+	private int voteCount;
+	
+	/*
+	 * Numero de companias involucradas en la produccion. 
+	 */
+	private int productionCompaniesNumber;
+	
+	/*
+	 * Numero de paises involucrados en la produccion
+	 */
+	private int productionCountriesNumber;
+	
+	/*
+	 * Numero de lenguajes hablados. 
+	 */
+	private int spokenLanguagesNumber;
 	
 	
 	
